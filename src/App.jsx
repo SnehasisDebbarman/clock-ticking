f1007e97724117230bff5f6a3e8be5f82ff6fe14
mktgsfdclightning <57293924+mktgsfdclightning@users.noreply.github.com>
2023-08-17 22:10:29 +0530

    jjjsjs

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,89 @@
+import { useState } from "react";
 import "./App.css";
-import DefaultCard from "./Card";
 
 function App() {
+  const [hour, setHour] = useState(new Date().getHours());
+  const [Minute, setMinute] = useState(new Date().getMinutes());
+  const [Second, setSecond] = useState(new Date().getMinutes());
+  setInterval(() => {
+    setHour(new Date().getHours());
+    setMinute(new Date().getMinutes());
+    setSecond(new Date().getSeconds());
+  }, 1000);
+
   return (
-    <div
-      style={{
-        display: "grid",
-        placeItems: "center",
-        height: "100vh",
-        width: "100vw",
-        overflow: "hidden",
-      }}
-    >
-      <div className={"dix"}></div>
+    <>
+      <div className="dix"></div>
       <div
         style={{
           zIndex: 10,
           width: "100vw",
-          height: "50vh",
-          // fontWeight: 700,
-          textAlign: "left",
+          position: "absolute",
+          height: "100vh",
+          alignItems: "flex-start",
         }}
       >
-        <h2 className="text-3xl font-bold ">Hello world!</h2>
+        <div
+          style={{
+            height: "80vh",
+            display: "flex",
+
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "flex-start",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              gap: "10px",
+              color: "rgb(176, 176, 176)",
+              fontWeight: "bolder",
+
+              fontSize: "70px",
+            }}
+          >
+            <p style={{ display: "flex", alignItems: "baseline" }}>
+              {hour}
+              <p
+                style={{
+                  fontSize: "20px",
+                }}
+              >
+                h
+              </p>
+            </p>
+            <p style={{ display: "flex", alignItems: "baseline" }}>
+              {Minute}
+              <p
+                style={{
+                  fontSize: "20px",
+                }}
+              >
+                m
+              </p>
+            </p>
+            <p style={{ display: "flex", alignItems: "baseline" }}>
+              {Second}
+              <p
+                style={{
+                  fontSize: "20px",
+                }}
+              >
+                s
+              </p>
+            </p>
+          </div>
+          <div
+            style={{
+              width: "400px",
+              height: "10px",
+              backgroundColor: "red",
+            }}
+          ></div>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
