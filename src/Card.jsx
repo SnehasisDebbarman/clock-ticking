f44a598cdbcb4d850928ade672ee63e2dbcc6810
Snehasis Debbarman <snehasisdebbarman2016@gmail.com>
2023-12-24 21:39:24 +0530

    Update Card.jsx

--- src/Card.jsx
+++ src/Card.jsx
@@ -10,6 +10,7 @@ export default function DefaultCard() {
         <p>
           Here are the biggest enterprise technology acquisitions of 2021 so
           far, in reverse chronological order.
+        hello clock 
         </p>
       </p>
     </Card>
